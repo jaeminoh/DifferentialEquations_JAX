d4b542bf9d780b555b9d6ceaf432776b9dbf6fe4
jaeminoh <jaeminoh.math@gmail.com>
2024-06-25 12:36:35 +0900

    modify julia code for heat equation

--- julia/HeatEquation.jl
+++ julia/HeatEquation.jl
@@ -4,16 +4,16 @@ Zero Boundary Condition
 Crank-Nicolson
 =#
 using LinearAlgebra
-using Plots
-using Serialization
+using Plots, LaTeXStrings, Format
 
 
 Δt = 0.001
-t1 = 3
+t1 = 1
 num_steps = Integer(t1 / Δt)
 N = 100
 Δx = 2 / N
-xx = Array(-1:Δx:1)
+xx = -1.0:Δx:1.0
+
 
 # initial condition
 u0 = zeros(Float64, size(xx))
@@ -26,7 +26,8 @@ A = diagm(
 )
 
 u_old = u0[2:end-1]
-ulist = [u0]
+ulist = [u_old]
+tlist = [0.0]
 
 function step(u)
     LHS = I(N - 1) - 0.5 * Δt / Δx^2 * A
@@ -34,18 +35,30 @@ function step(u)
     return LHS \ b
 end
 
-for i in 1:1:num_steps
-    u_new = step(u_old)
-    global u_old = u_new
-    if i % 10 == 0
-        push!(ulist, u_new)
+function solve!(ulist, tlist, num_steps)
+    u_old = ulist[end]
+    t = tlist[end]
+    for i in 1:1:num_steps
+        u_new = step(u_old)
+        u_old = u_new
+        t += Δt
+        if i % 10 == 0
+            push!(ulist, u_new)
+            push!(tlist, t)
+        end
     end
 end
 
+solve!(ulist, tlist, num_steps)
 
-anim = @animate for u ∈ ulist
-    fig = plot(legend=false)
-    plot!(fig, u, ylim=(-0.1, 1.1))
+
+anim = @animate for (u, t) ∈ zip(ulist, tlist)
+    plot(xx, [0; u; 0], label="\$u_t = u_{xx}\$", dpi=300)
+    xlims!(-1, 1)
+    ylims!(-0.1, 1.1)
+    xlabel!(L"x")
+    ylabel!(L"u")
+    title!("\$t=$(format(t, precision=2))\$")
 end
 
-gif(anim, "HeatEquation.gif", fps=60)
+gif(anim, "figures/HeatEquation.gif", fps=30)
